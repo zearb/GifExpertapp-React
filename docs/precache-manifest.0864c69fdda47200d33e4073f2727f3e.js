6fd21b11a0a8ac134a5b16b963ab11d63d3ca504
Aldo Ráez <al2_ze@outlook.es>
2020-07-23 22:03:12 -0500

    build renombrado a docs

---
+++ docs/precache-manifest.0864c69fdda47200d33e4073f2727f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eaf37a45e7d1dbd1cb7a0f29776ed7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0b9c71102085b80561",
+    "url": "/static/css/main.583aa03a.chunk.css"
+  },
+  {
+    "revision": "2c7275a2411ef8944e9b",
+    "url": "/static/js/2.5163bc94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5163bc94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0b9c71102085b80561",
+    "url": "/static/js/main.902c1ce6.chunk.js"
+  },
+  {
+    "revision": "4d3c5aad2ee9da455662",
+    "url": "/static/js/runtime-main.a4f11275.js"
+  }
+]);
